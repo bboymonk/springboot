ca721870b178330ddf902934acae0f76bbcb52aa
bboymonk <1523156110@qq.com>
2019-06-05 16:02:53 +0800

    验证码

--- src/main/java/com/wjb/controller/CaptchaController.java
+++ src/main/java/com/wjb/controller/CaptchaController.java
@@ -4,7 +4,6 @@ import com.wjb.base.BaseController;
 import com.wjb.util.CreateImageCode;
 import com.wjb.util.StaticFinalParam;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
